66e15588a6b7543df0c845edfa868a03ecde2089
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-03 15:15:52 +0530

    passing handlers

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
 
+const Selection = ({applyColor}) => {
+  const [backgroundColorStyle,setBackgroundColorStyle]=useState({background:""});
+  return (
+    <div
+      className={"fix-box"}
+      style={{
+        background: {},
+        borderWidth: "1px",
+        borderStyle: "solid",
+        borderColor: "black",
+        borderRadius: "5px",
+        height: "300px",
+        width: "300px",
+        textAlign: "center",
+        color: "white",
+        backgroundColor:backgroundColorStyle.background
+      }}
+      onClick={()=>applyColor(setBackgroundColorStyle)}
+    >
+      <h3>Selection</h3>
+    </div>
+  );
+};
+
+export default Selection;
